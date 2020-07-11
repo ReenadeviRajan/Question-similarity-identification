086cf3ebf79463cc7bff069c0bd931eaf7b81f83
Reenadevi R <41472124+ReenadeviRajan@users.noreply.github.com>
2020-07-11 15:41:57 +0530

    Update Image_to_data && latex.py

--- Image_to_data && latex.py
+++ Image_to_data && latex.py
@@ -1,11 +1,8 @@
 from sympy import *
 from latex2sympy import *
 from sympy.parsing.latex import parse_latex
-x=symbols("x")
-print(latex(1/( (x+2)*(x+1) )))
-expr = parse_latex(r"\frac {1 + \sqrt {\a}} {\b}")
-print(expr)
-expr = sympy.parsing.latex.parse_latex(r"\frac {1 + \sqrt {\a}} {\b}")
+x=input()
+expr = sympy.parsing.latex.parse_latex(x)
 print(sympy.printing.latex(expr))
 
 cnt=0
@@ -14,4 +11,4 @@ def open_img():
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
     k = pytesseract.image_to_string(x)
     ask_question(1 , k)
-
+open_img()
